cbd0dfad55b7e9c935e12c4b755bda9cb7f53c4f
Lukas May <jlukasmay@gmail.com>
2023-12-02 16:02:54 -0500

    Part 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,19 +1,5 @@
 import re
-
-def part1(input_file):
-    sum  = 0
-    with open(input_file) as file:
-        for line in file:
-            first = str(re.findall(r"\d", line)[0])
-            last = str(re.findall(r"\d", line)[-1])
-            sum += int(first + last)
-
-    return sum
-
-def part2(input_file):
-    sum = 0
-    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    convert = {
+CONVERT = {
         'one': '1',
         'two': '2',
         'three': '3',
@@ -34,30 +20,32 @@ def part2(input_file):
         '8': '8',
         '9': '9',
         '0': '0'
-    }
+}
+
+def part1(input_file):
+    sum  = 0
+    with open(input_file) as file:
+        for line in file:
+            first = str(re.findall(r"\d", line)[0])
+            last = str(re.findall(r"\d", line)[-1])
+            sum += int(first + last)
+
+    return sum
+
+def part2(input_file):
+    sum = 0
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     with open(input_file) as file:
         for line in file:
             indexes = []
             for i in range(len(words)):
-                if not line.find(words[i]) == -1:
-                    indexes.append(words[i])
-            sum += int(convert.get(indexes[0]) + convert.get(indexes[-1]))
+                a = [m.start() for m in re.finditer(words[i], line)]
+                for z in a:
+                    indexes.append((z, words[i]))
+            indexes.sort()
+            sum += int(CONVERT.get(str(indexes[0][1])) + CONVERT.get(str(indexes[-1][1])))
 
     return sum
-
-def convert_written_numbers(text):
-  # Create a regular expression to match written numbers.
-  number_regex = re.compile(r'\d+')
-
-  # Find all written numbers in the text.
-  numbers = number_regex.findall(text)
-
-  # Convert each written number to a number.
-  for number in numbers:
-    text = text.replace(number, str(int(number)))
-
-  # Return the converted text.
-  return text
 
 def main():
     input_file = "/Users/lukasmay/git/adventofcode2023/Input/day1.txt"
